08edbf820dcc1549004326dbbb2c85fa3dd949cd
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-26 14:28:17 +0100

    pass name via props and handle onchange event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
+'use client'
+import { useState } from 'react'; 
 import Header from "@/components/Header";
 import MovieCard from "@/components/MovieCard";
-import { Input } from "@/components/ui/input"
-import 'remixicon/fonts/remixicon.css'
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import "remixicon/fonts/remixicon.css";
 
 export default function Home() {
-  
+  const [movieName, setMovieName] = useState(''); 
+ 
   return (
-   <>
-  <Header />
+    <>
+      <Header />
+      <div className="relative w-[90%] md:w-[50%] mx-auto -mt-6">
+        <i className="ri-search-2-line text-primary font-bold absolute top-1/2 left-2 transform -translate-y-1/2"></i>
+        <Input
+          placeholder="Search for movie"
+          type="text"
+          className="pl-10  py-2 border rounded-lg w-full"
+          value={movieName}
+          onChange={(e) => setMovieName(e.target.value)} 
+        />
+      </div>
 
-  <div className="relative w-[90%] md:w-[50%] mx-auto -mt-6">
-            <i className="ri-search-2-line text-primary font-bold absolute top-1/2 left-2 transform -translate-y-1/2"></i>
-            <Input
-              placeholder="Search for movie"
-              type="text"
-              className="pl-10 pr-3 py-2 border rounded-lg w-full"
-            />
-          </div>
-      
-  <MovieCard />
-   </>
+    {movieName && <MovieCard movieName={movieName} />}
+    </>
   );
 }
