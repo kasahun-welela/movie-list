aa3deff7e0478303705be8b903727d4764417c90
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-30 13:31:57 +0300

    add  unique movie to local storage

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   Card,
   CardContent,
@@ -7,6 +7,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "./ui/button";
+import { toast } from "react-toastify";
 
 interface Movie {
   imdbID: string;
@@ -24,21 +26,62 @@ function MovieCard(props: { movieName: string }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://www.omdbapi.com/?t=${props.movieName}&apikey=e4b23aff`);
-        console.log(response)
-        setMovieData([response.data]); 
+        const response = await axios.get(
+          `https://www.omdbapi.com/?t=${props.movieName}&apikey=e4b23aff`
+        );
+        console.log(response);
+        setMovieData([response.data]);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, [props.movieName]); 
+  }, [props.movieName]);
+
+  const addMovie = (
+    imdbID: string,
+    Poster: string,
+    Title: string,
+    imdbRating: string,
+    Runtime: string,
+    Genre: string,
+    Plot: string
+  ) => {
+    const movie = {
+      imdbID: imdbID,
+      Poster: Poster,
+      Title: Title,
+      imdbRating: imdbRating,
+      Runtime: Runtime,
+      Genre: Genre,
+      Plot: Plot,
+    };
+    console.log("film");
+
+    const existingMovies = JSON.parse(localStorage.getItem("movies") || "[]");
+    const isDuplicate = existingMovies.some(
+      (movie: any) => movie.imdbID === imdbID
+    );
+
+    if (!isDuplicate) {
+      existingMovies.push(movie);
+      localStorage.setItem("movies", JSON.stringify(existingMovies));
+      console.log("Movie added to watchlist:", movie);
+      toast.success("Movie added to watchlist");
+    } else {
+      console.log("Movie already exists in watchlist:", movie);
+      toast.error("Movie already exists in watchlist");
+    }
+  };
 
   return (
     <>
-      {movieData.map(movie => (
-        <Card key={movie.imdbID} className="w-[90%] md:w-[50%] mx-auto flex flex-col md:flex-row my-3">
+      {movieData.map((movie) => (
+        <Card
+          key={movie.imdbID}
+          className="w-[90%] md:w-[50%] mx-auto flex flex-col md:flex-row my-3"
+        >
           <img
             src={movie.Poster}
             className="md:h-48 h-[200px] rounded-t-md md:rounded-r-none md:rounded-l-md w-full md:w-fit"
@@ -49,17 +92,32 @@ function MovieCard(props: { movieName: string }) {
               <div className="flex gap-4">
                 <CardTitle>{movie.Title}</CardTitle>{" "}
                 <p>
-                  <i className="ri-star-fill text-yellow-400"></i> {movie.imdbRating}
+                  <i className="ri-star-fill text-yellow-400"></i>{" "}
+                  {movie.imdbRating}
                 </p>
               </div>
               <CardDescription className="flex max-w-sm gap-3">
-                <p>{movie.Runtime}</p> <p>{movie.Genre}</p> <p>Watchlist</p>
+                <p>{movie.Runtime}</p> <p>{movie.Genre}</p>{" "}
+                <p
+                  onClick={() =>
+                    addMovie(
+                      movie.imdbID,
+                      movie.Poster,
+                      movie.Title,
+                      movie.imdbRating,
+                      movie.Runtime,
+                      movie.Genre,
+                      movie.Plot
+                    )
+                  }
+                >
+                  <i className="ri-add-circle-fill"></i>
+                  Watchlist
+                </p>
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <p className="text-justify ">
-                {movie.Plot}
-              </p>
+              <p className="text-justify ">{movie.Plot}</p>
             </CardContent>
           </div>
         </Card>
